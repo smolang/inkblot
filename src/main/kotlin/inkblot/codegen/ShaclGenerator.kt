3ef690f5ec3df13bffc878c6d9193d3156742cd2
rec0de <fenghuang@rec0de.net>
2023-02-09 11:46:44 +0100

    detect imprecise SHACL

--- src/main/kotlin/inkblot/codegen/ShaclGenerator.kt
+++ src/main/kotlin/inkblot/codegen/ShaclGenerator.kt
@@ -10,11 +10,42 @@ object ShaclGenerator {
         val shapeUri = "http://rec0de.net/ns/inkblot#SHACL${Inkblot.freshSuffixFor("SHACL")}"
         val shapeStatements = mutableListOf("a sh:NodeShape", "sh:targetClass <$targetClassUri>")
 
+        checkForWeirdness(className, variableInfo.values, paths)
+
         variableInfo.values.forEach {
             shapeStatements.add(genPropertyShape(className, it, paths.pathsTo(it.sparqlName)))
         }
 
         return "<$shapeUri> ${shapeStatements.joinToString("; ")}."
+    }
+
+    // SPARQL happens to be annoyingly expressive
+    // in a decent amount of cases, the SHACL we can generate does not quite match the actual restrictions
+    // (i think usually the SHACL will just be overly restrictive, but there might be edge cases where it's just wrong sometimes)
+    // therefore we'll try to determine how trustworthy our SHACL is by detecting SPARQL weirdness
+    private fun checkForWeirdness(className: String, variableInfo: Collection<VariableProperties>, paths: VariablePathAnalysis) {
+        val concreteLeaves = paths.concreteLeaves().map { paths.pathsToConcrete(it) }
+
+        // we expect one concrete leaf to specify the type of what we select - this is fine and can be mapped to SHACL
+        val relevantLeaves = concreteLeaves.filterNot { it.size == 1 && it.first().size == 1 && it.first().first().dependency.p == "http://www.w3.org/2000/01/rdf-schema#type" }.flatten()
+
+        if(relevantLeaves.isNotEmpty())
+            println("WARNING: Query contains constant leaves, generated SHACL for '$className' might match instances it shouldn't")
+
+        val propertyPaths = variableInfo.flatMap {
+            val p = paths.pathsTo(it.sparqlName)
+            if(p.size > 1)
+                println("WARNING: Query contains several paths to '?${it.sparqlName}', generated SHACL for '$className.${it.targetName}' might be too restrictive")
+            p
+        }
+
+        propertyPaths.forEach { pPath ->
+            if(relevantLeaves.any{ cPath -> pPath.toSet().intersect(cPath.toSet()).isNotEmpty() }) {
+                val last = pPath.last()
+                val v = if(last.backward) last.dependency.s else last.dependency.o
+                println("WARNING: Query contains concrete leaf along path to '?$v', generated SHACL might be too restrictive")
+            }
+        }
     }
 
     private fun genPropertyShape(className: String, v: VariableProperties, paths: List<List<VarDepEdge>>): String {
@@ -41,9 +72,6 @@ object ShaclGenerator {
     }
 
     private fun genShaclPath(v: String, paths: List<List<VarDepEdge>>): String {
-        if(paths.size > 1)
-            println("WARNING: More than one path leads to ?$v, generated SHACL constraints may be wonky")
-
         val shaclPaths = paths.map { singlePathToShacl(it) }
 
         return if(shaclPaths.size > 1)

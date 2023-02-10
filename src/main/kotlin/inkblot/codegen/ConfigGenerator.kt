ce43acd9a038014ee2c769cbbf412c797251c9df
rec0de <fenghuang@rec0de.net>
2023-02-10 11:30:44 +0100

    change default generated type config

--- src/main/kotlin/inkblot/codegen/ConfigGenerator.kt
+++ src/main/kotlin/inkblot/codegen/ConfigGenerator.kt
@@ -17,7 +17,7 @@ object ConfigGenerator {
             classType = ct ?: classType
             pr
         }
-        else (query.resultVars.toSet() - anchor).associateWith { PropertyConfig(it, "Unit", "*") }
+        else (query.resultVars.toSet() - anchor).associateWith { PropertyConfig(it, "http://example.com/ns/any", "*") }
 
         val classConfig = ClassConfig(anchor, classType, prettifySparql(query), null, properties)
         return Pair(className, classConfig)
